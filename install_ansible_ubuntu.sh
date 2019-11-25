d8283b4523af701172137cc543354e2b8e4200d9
Rory McCune <rorym@mccune.org.uk>
2019-11-25 17:59:39 +0000

    update ubuntu ansible install script

--- install_ansible_ubuntu.sh
+++ install_ansible_ubuntu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install software-properties-common && sudo apt-add-repository --yes --update ppa:ansible/ansible && sudo apt install ansible
+sudo apt update && sudo apt install -y software-properties-common && sudo apt-add-repository --yes --update ppa:ansible/ansible && sudo apt install -y ansible
